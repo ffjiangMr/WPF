4ec2c6ce9ff64bf58e1918754ab22b5135114e5b
feifei.jiang@outlook.com <Feifei.Jiang@outlook.com>
2018-09-04 16:25:54 +0800

    update

--- MyFirstWpfApplication/MyFirstWpfApplication/MainWindow.xaml.cs
+++ MyFirstWpfApplication/MyFirstWpfApplication/MainWindow.xaml.cs
@@ -25,11 +25,26 @@ namespace MyFirstWpfApplication
         public MainWindow()
         {
             InitializeComponent();
-            stu = new Student();
 
-            Binding binding = new Binding();
-            binding.Source = stu;
-            binding.Path = new PropertyPath("Name");
+            List<Student> stuList = new List<Student>()
+            {
+                new Student(){ID=0,Name="Tim", Age=29, },
+                new Student(){ID=2,Name="Tom", Age=27, },
+                new Student(){ID=3,Name="Kyle", Age=26, },
+                new Student(){ID=4,Name="Tony", Age=25, },
+                new Student(){ID=5,Name="Mike", Age=24, },
+            };
+
+            //this.listBoxStudents.ItemsSource = stuList;
+            //this.listBoxStudents.DisplayMemberPath = "Name";            
+            //Binding binding = new Binding("SelectedItem.ID") { Source = this.listBoxStudents };
+            //this.textBoxId.SetBinding(TextBox.TextProperty, binding);
+
+            //stu = new Student();
+
+            //Binding binding = new Binding();
+            //binding.Source = stu;
+            //binding.Path = new PropertyPath("Name");
 
             //BindingOperations.SetBinding(this.textBoxName, TextBox.TextProperty, binding);
         }
@@ -37,7 +52,15 @@ namespace MyFirstWpfApplication
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             //this.textBox.Text = this.FindResource("myString") as String;
-            stu.Name += "Name";
+            List<Student> stuList = new List<Student>()
+            {
+                new Student(){ID=0,Name="Tim", Age=29, },
+                new Student(){ID=2,Name="Tom", Age=27, },
+                new Student(){ID=3,Name="Kyle", Age=26, },
+                new Student(){ID=4,Name="Tony", Age=25, },
+                new Student(){ID=5,Name="Mike", Age=24, },
+            };
+            this.listViewStudents.ItemsSource = from stu in stuList where stu.Name.StartsWith("T") select stu;
         }
     }
 }
--- MyFirstWpfApplication/MyFirstWpfApplication/Student.cs
+++ MyFirstWpfApplication/MyFirstWpfApplication/Student.cs
@@ -4,24 +4,27 @@ using System.Threading;
 
 namespace MyFirstWpfApplication
 {
-    public class Student : INotifyPropertyChanged
+    public class Student /*: INotifyPropertyChanged*/
     {
-        private String name;
+        //private String name;
+        public Int32 ID { get; set; }
+        public String Name { get; set; }
+        public Int32 Age { get; set; }
 
-        public String Name
-        {
-            get { return this.name; }
-            set
-            {
-                this.name = value;
-                PropertyChangedEventHandler temp = Volatile.Read(ref this.PropertyChanged);
-                if (temp != null)
-                {
-                    temp(this, new PropertyChangedEventArgs("Name"));
-                }
-            }
-        }
+        //public String Name
+        //{
+        //    get { return this.name; }
+        //    set
+        //    {
+        //        this.name = value;
+        //        PropertyChangedEventHandler temp = Volatile.Read(ref this.PropertyChanged);
+        //        if (temp != null)
+        //        {
+        //            temp(this, new PropertyChangedEventArgs("Name"));
+        //        }
+        //    }
+        //}
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        //public event PropertyChangedEventHandler PropertyChanged;
     }
 }
